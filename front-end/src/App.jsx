f4a861ec7aa0de7fce7d72ec1fbce2977542ebc5
juleklazura <gabrieljulek990@gmail.com>
2022-08-12 15:58:26 -0300

    Estilização do site e modificações no vite.config.js

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -11,17 +11,15 @@ function App() {
 
   return (
     <div>
-      <Favicon src="src/images/favicon.png"></Favicon>
+      <Favicon src="https://lh3.googleusercontent.com/pw/AL9nZEWQlHchilg8nTGwlUSbokoX1w-KzodbMv_CvwxAeEUJ4Wa0YJOFpPMXYTRFDVPN0A2yI7tIY2Gg4pVKaHjV9jV-oexm_4bHyYCAECh5t3LvHe8WIE2ukYURXDYj4xo8DkmgQaJ-YEMtU2fKsxDApLU=w500-h250-no?authuser=0"></Favicon>
       <BrowserRouter>
-      <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+      <nav className="navbar navbar-expand-lg ">
           <div className="container">
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <Link to={'/jogos/list'} className="navbar-brand">
-              <b>
-                Ciro Games
-              </b>
+              <img classs="img-responsive" width="130px" height="" src="https://lh3.googleusercontent.com/pw/AL9nZEWQlHchilg8nTGwlUSbokoX1w-KzodbMv_CvwxAeEUJ4Wa0YJOFpPMXYTRFDVPN0A2yI7tIY2Gg4pVKaHjV9jV-oexm_4bHyYCAECh5t3LvHe8WIE2ukYURXDYj4xo8DkmgQaJ-YEMtU2fKsxDApLU=w500-h250-no?authuser=0"/>
             </Link>
             <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
               <ul className='navbar-nav'>
@@ -29,18 +27,17 @@ function App() {
                   <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Listar
                   </a>
-                  <ul className="dropdown-menu dropdown-menu-light bg-danger" aria-labelledby="navbarDarkDropdownMenuLink">
+                  <ul className="dropdown-menu " aria-labelledby="navbarDarkDropdownMenuLink">
                     <Link to={"/jogos/list"} className="dropdown-item">
                       Jogos
                     </Link>
-                  
                   </ul>
                 </li>
                 <li className="nav-item dropdown">
                   <a className="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Adicionar
                   </a>
-                  <ul className="dropdown-menu dropdown-menu-light bg-danger" aria-labelledby="navbarDarkDropdownMenuLink">
+                  <ul className="dropdown-menu " aria-labelledby="navbarDarkDropdownMenuLink">
                     <Link to={"/jogos/add"} className="dropdown-item">
                       Jogos
                     </Link>

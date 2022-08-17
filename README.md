071cf1d1464c472a3fb3f701eac81745b9b17334
Rafael Strack <strackinado@gmail.com>
2022-08-16 23:55:45 -0300

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,39 @@
 Desenvolvimento Web - Trabalho 03 
 
 Grupo: Gabriel Julek Klazura, Gustavo Chagas Andrade e Rafael Strack
+
+rotas api:
+
+get - listar todos os jogos:
+http://localhost:8080/api/jogos
+
+get - listar jogos por título:
+http://localhost:8080/api/jogos&titulo=[titulo]
+
+post - criar jogo:
+http://localhost:8080/api/jogos
+
+body:   {
+            "titulo": ""
+            "valor": ""
+            "genero": ""
+        }
+
+get - listar jogo dado o id:
+http://localhost:8080/api/jogos/:id
+
+put - editar dados de um jogo:
+http://localhost:8080/api/jogos/:id
+
+body:   {
+            "titulo": ""
+            "valor": ""
+            "genero": ""
+        }
+
+delete - remover jogo dado o id:
+http://localhost:8080/api/jogos/:id
+
+delete - remover todos os jogos:
+http://localhost:8080/api/jogos/
+
--- back-end/src/main/java/br/com/api/jogos_gui/controller/JogoController.java
+++ back-end/src/main/java/br/com/api/jogos_gui/controller/JogoController.java
@@ -31,7 +31,7 @@ public class JogoController {
     JogoRepository rep;
 
     /*
-     * GET /api/jogos : listar todos os jogos
+     * GET /api/jogos : listar todos os jogos ou listar por título
      */
     @GetMapping("/jogos")
     public ResponseEntity<List<Jogo>> getAllJogos(@RequestParam(required = false) String titulo) {
@@ -54,7 +54,7 @@ public class JogoController {
     }
 
     /*
-     * POST /api/jogos : criar artigo
+     * POST /api/jogos : criar jogo
      */
     @PostMapping("/jogos")
     public ResponseEntity<Jogo> createJogo(@RequestBody Jogo jogo) {
@@ -70,7 +70,7 @@ public class JogoController {
     }
 
     /*
-     * GET /api/jogos/:id : listar artigo dado um id
+     * GET /api/jogos/:id : listar jogo dado um id
      */
     @GetMapping("/jogos/{id}")
     public ResponseEntity<Jogo> getJogoById(@PathVariable("id") Long id) {
@@ -83,7 +83,7 @@ public class JogoController {
     }
 
     /*
-     * PUT /api/jogos/:id : atualizar artigo dado um id
+     * PUT /api/jogos/:id : atualizar jogo dado um id
      */
     @PutMapping("/jogos/{id}")
     public ResponseEntity<Jogo> updateJogo(@PathVariable("id") Long id, @RequestBody Jogo j) {
@@ -102,7 +102,7 @@ public class JogoController {
     }
 
     /*
-     * DEL /api/jogos/:id : remover artigo dado um id
+     * DEL /api/jogos/:id : remover jogo dado um id
      */
     @DeleteMapping("/jogos/{id}")
     public ResponseEntity<HttpStatus> deleteJogo(@PathVariable("id") Long id) {
